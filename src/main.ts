0299bf38b4d0266801eef0e77a9a3736a61b3a86
Mats Hagen <mats.hagen2@gmail.com>
2023-07-04 09:07:20 +0200

    environment variables

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
-      origin: 'http://localhost:5173',
+      origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
       methods: 'GET, PUT, PATCH, POST, DELETE',
       credentials: true,
     },
   });
-  await app.listen(3000);
+  await app.listen(port).then(() => {
+    console.log(`Server started on port ${port}`);
+  });
 }
 bootstrap();
