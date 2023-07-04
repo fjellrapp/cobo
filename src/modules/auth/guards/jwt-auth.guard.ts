e9998155946975fd0b73b4c9bdd4d4e3a2ae565c
Mats Hagen <mats.hagen2@gmail.com>
2023-07-04 10:07:49 +0200

    conflict

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -6,7 +6,6 @@ import {
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
-import { IS_PUBLIC_KEY } from '@/common/utils/decorators/public';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
